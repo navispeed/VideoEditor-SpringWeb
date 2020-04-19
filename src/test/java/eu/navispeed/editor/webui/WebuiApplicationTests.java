a7158f92f875945613de5bd0f296032ea47f195f
Gregoire Guémas <gregoire.guemas@navispeed.eu>
2020-04-19 13:07:27 +0200

    Moving to correct repository WebUi

---
+++ src/test/java/eu/navispeed/editor/webui/WebuiApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.navispeed.editor.webui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
